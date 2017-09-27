45dddfbd268481cf8f61ad7035fda485832a7c01
Tyler Tackett <ttackett412@yahoo.com>
2017-09-27 13:56:08 -0400

    Committing the final project

--- main.cpp
+++ main.cpp
@@ -1,25 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <iostream>
+#include <string>
 
-/* 
- * File:   main.cpp
- * Author: ttackett
- *
- * Created on September 25, 2017, 2:42 PM
- */
+class AdHocSquare {
+    
+public:
+    
+    AdHocSquare(double side){this->side = side;}
+    void set_side(double side){this->side = side;}
+    double get_area();
+    
+private:
+    
+    double side;
+};
 
-#include <cstdlib>
+class LazySquare {
+    
+public:
+    
+    LazySquare(double side){this->side = side;}
+    void set_side(double side){this->side = side;}
+    double get_area();
 
-using namespace std;
+private: 
+    double side;
+    double area;
+    bool side_changed;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+};
 
-    return 0;
+double AdHocSquare::get_area()
+{
+    return side * side;
+}
+
+double LazySquare::get_area()
+{
+    if(area == 0)
+    {
+        area = side * side;
+    }
+    else
+    { 
+        if(area != side * side)
+        {
+            side_changed = true;
+        }
+        else
+        {
+            side_changed = false;
+        } 
+        
+        if(side_changed == true)
+        {
+            area = side * side;
+        } 
+    }
+    return area;
+}
+
+int main(void) {
+    AdHocSquare AdHocSquare(5.5);
+    std::cout << "The AdHocSquare area is " << AdHocSquare.get_area() << std::endl;
+    
+    AdHocSquare.set_side(6.5);
+    std::cout << "The new AdHocSquare area is " << AdHocSquare.get_area() << std::endl;
+    
+    LazySquare LazySquare(4.5);
+    std::cout << "The LazySquare area is " << LazySquare.get_area() << std::endl;
+    
+    LazySquare.set_side(3.5);
+    std::cout << "The new LazySquare area is " << LazySquare.get_area() << std::endl;
 }
 
