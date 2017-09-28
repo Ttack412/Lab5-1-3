311798a62158f5ef27f9bf3696b61ce86adb4141
Tyler Tackett <ttackett412@yahoo.com>
2017-09-28 14:45:55 -0400

    Committing the progress of the project

--- main.cpp
+++ main.cpp
@@ -29,19 +29,24 @@ private:
 
 };
 
+//multiplies the sides of the square
 double AdHocSquare::get_area()
 {
     return side * side;
 }
 
+
 double LazySquare::get_area()
 {
+    //checks the area
     if(area == 0)
     {
+        //sets the area if it's not already set
         area = side * side;
     }
     else
     { 
+        //if the area isn't equal to the sides, reset it
         if(area != side * side)
         {
             side_changed = true;
@@ -60,15 +65,19 @@ double LazySquare::get_area()
 }
 
 int main(void) {
+    //creates an AdHocSquare and sets its side
     AdHocSquare AdHocSquare(5.5);
     std::cout << "The AdHocSquare area is " << AdHocSquare.get_area() << std::endl;
     
+    //resets the side
     AdHocSquare.set_side(6.5);
     std::cout << "The new AdHocSquare area is " << AdHocSquare.get_area() << std::endl;
     
+    //creates an LazySquare and sets its side
     LazySquare LazySquare(4.5);
     std::cout << "The LazySquare area is " << LazySquare.get_area() << std::endl;
     
+    //resets the side
     LazySquare.set_side(3.5);
     std::cout << "The new LazySquare area is " << LazySquare.get_area() << std::endl;
 }
